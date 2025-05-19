9153d805061f5eca1730c8d6a614ba749caa2538
hacene.zerrouk@gmail.com <hacene.zerrouk@gmail.com>
2025-05-19 16:44:08 +0200

    clean data

--- model_dl.py
+++ model_dl.py
@@ -3,5 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, RobustScaler, OneHotEncoder,OrdinalEncoder, LabelEncoder
 
+
+# Bonne mise en forme des noms de colonnes
 df = pd.read_csv('dataset.csv')
-df.columns = [c.strip() for c in df.columns]
+df.columns = [c.strip().lower() for c in df.columns]
