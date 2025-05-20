c0f250ea9a6a638161d13ff786a7efcaf85ed4c3
hacene.zerrouk@gmail.com <hacene.zerrouk@gmail.com>
2025-05-20 12:18:38 +0200

    test_modele

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,20 +1,20 @@
 import tensorflow as tf
 
-def build_model():
+def build_model(X_train, num_classes):
     # Réseau avec 2 couches cachées de 64 neurones chacune
     # et une couche de sortie avec activation softmax pour classification
     model = tf.keras.Sequential([
         tf.keras.layers.Input(shape=(X_train.shape[1],)),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(num_classes, activation='softmax')
+        tf.keras.layers.Dense(64, activation='sigmoid'),
+        tf.keras.layers.Dense(64, activation='sigmoid'),
+        tf.keras.layers.Dense(num_classes, activation='sigmoid')
     ])
 
     # Définition de la fonction de perte, de l'optimiseur et des métriques
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
         loss='categorical_crossentropy',
-        metrics=['accuracy']
+        metrics=['accuracy',tf.keras.metrics.AUC(name = "auc")]
     )
 
     model.summary()
