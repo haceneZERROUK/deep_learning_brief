9124e25eb85824a9cc5c37e3e7885dad65761115
hacene.zerrouk@gmail.com <hacene.zerrouk@gmail.com>
2025-05-20 16:37:34 +0200

    gridsearch test 500 modele

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,14 +5,14 @@ def build_model(X_train, num_classes):
     # et une couche de sortie avec activation softmax pour classification
     model = tf.keras.Sequential([
         tf.keras.layers.Input(shape=(X_train.shape[1],)),
-        tf.keras.layers.Dense(64, activation='sigmoid'),
-        tf.keras.layers.Dense(64, activation='sigmoid'),
+        tf.keras.layers.Dense(8, activation='relu'),
+        tf.keras.layers.Dense(8, activation='sigmoid'),
         tf.keras.layers.Dense(num_classes, activation='sigmoid')
     ])
 
     # Définition de la fonction de perte, de l'optimiseur et des métriques
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.003),
         loss='categorical_crossentropy',
         metrics=['accuracy',tf.keras.metrics.AUC(name = "auc")]
     )
